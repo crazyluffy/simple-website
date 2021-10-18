6b71b6d64935663a2065e43c77fae41431241133
renming <renming1414@sina.com>
2021-10-18 12:33:26 +0800

    change frontend project name in package.json; init backend

---
+++ backend/src/main/java/org/renm/Boot.java
@@ -0,0 +1,11 @@
+package org.renm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
